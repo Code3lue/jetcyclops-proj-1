138be44bc2e0d42481fe245b01ed967642cc05ee
code3lue <alan.delez@gmail.com>
2017-03-21 19:59:20 -0400

    wtf

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,3 +3,20 @@ $(document).ready(function() {
 	$('#myModal').modal('show');
 
 });
+
+$(function(){
+       $("#exampleInputName2").prop('required',true);
+       $("#exampleInputEmail2").prop('required', true);
+});
+
+$(".startButton").on("click", function() {
+
+	event.preventDefault();
+
+	$('#myModal').modal('hide');
+
+});
+
+
+
+
